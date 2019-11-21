68f748ad5feec74dd38f1bf1c10d8b6fc71298eb
d3vin-v <devinje01@gmail.com>
2019-11-21 12:48:04 -0600

    beginning

---
+++ DevinBrain.java
@@ -0,0 +1,10 @@
+
+public class DevinBrain implements Brain {
+
+	@Override
+	public Move bestMove(Board board, Piece piece, int limitHeight, Move move) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
